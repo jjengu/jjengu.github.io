bb5a52defbccdaefb38ad51b93895b990443bbb8
jengu <124980843+Pstrw@users.noreply.github.com>
2024-06-27 06:17:18 +0300

    Update solaris.lua

--- scripts/solaris/solaris.lua
+++ scripts/solaris/solaris.lua
@@ -1,7 +1,7 @@
 local games = {
-    [6361937392] = "https://raw.githubusercontent.com/Pstrw/private/main/games/stfobtb.lua",
-    [6356763358] = "https://raw.githubusercontent.com/Pstrw/private/main/games/stfobtb.lua",
-    [11515893037] = "https://raw.githubusercontent.com/Pstrw/private/main/games/stfobtb.lua",
+    [6361937392] = "https://raw.githubusercontent.com/Pstrw/Pstrw.github.io/main/scripts/solaris/games/stfobtb.lua",
+    [6356763358] = "https://raw.githubusercontent.com/Pstrw/Pstrw.github.io/main/scripts/solaris/games/stfobtb.lua",
+    [11515893037] = "https://raw.githubusercontent.com/Pstrw/Pstrw.github.io/main/scripts/solaris/games/stfobtb.lua",
     [10191847911] = "https://raw.githubusercontent.com/Pstrw/private/main/games/stfo.lua"
 }
 
